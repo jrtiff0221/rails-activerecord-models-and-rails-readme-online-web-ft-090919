b640caf61143d5c4a1e27d8737a94849cc054b00
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-11-13 17:51:45 +0000

    Automatically backed up by Learn

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module RailsActiverecordModelsAndRailsReadme
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.i18n.fallbacks = true
+    config.public_file_server.enabled = true
   end
 end
